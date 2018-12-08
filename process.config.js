a5893d92a6da79e792420c69d1102bbb989be64d
Jesus Bibieca <jbibieca@intellisys.com.do>
2018-12-08 15:33:47 -0400

    tst

--- process.config.js
+++ process.config.js
@@ -15,17 +15,9 @@ module.exports = {
     },
     env_production: {
       NODE_ENV: 'production'
+    },
+    env_hook: {
+      command: 'git pull origin master && npm i && npm test && pm2 restart "My APP"'
     }
-  }],
-
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+  }]
 };
